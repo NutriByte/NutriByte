86df671b645feda8b9cdfecea4e32c264829f937
bryanoid9 <bryanoid9@gmail.com>
2025-02-16 09:59:04 -0800

    Added api endpoint

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+@app.route("/api/message")
+def message():
+    return jsonify({"message": "This is my first API response!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
