a7c1229e34a58c3d51d5dae61f8df1e9091fe93a
bryanoid9 <bryanoid9@gmail.com>
2025-02-16 12:12:51 -0800

    added the buttons, new format, new page

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/team')
+def team():
+    return render_template('team_page.html')
+
 @app.route('/api/message')
 def get_message():
     return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    print(f"Running on http://127.0.0.1:{port}/")  # Add this line
+    print(f"Running on http://127.0.0.1:{port}/")  
     app.run(host='127.0.0.1', port=port, debug=True)
